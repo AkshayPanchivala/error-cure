780c6e2d8b1c89a2f76eb853c178d4c385688d50
Akshay Panchivala <akshaypanchivala@gmail.com>
2024-12-24 22:38:18 +0530

    fixed one import issue

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const AppError = require('./errors/AppError');
 const ValidationError = require('./errors/ValidationError');
 const AuthError = require('./errors/AuthError');
-const DatabaseError = require('./errors/DatabaseError');
+const DatabaseError = require('./errors/DataBaseError');
 const NotFoundError = require('./errors/NotFoundError');
 const globalErrorHandler = require('./middleware/globalErrorHandler');
 const handleUnhandledRejections=require("./utils/handleRejections");
